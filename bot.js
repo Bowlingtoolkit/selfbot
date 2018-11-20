5d4431ec8f1c1375615d96039f23ae40faf95416
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-11-20 20:32:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,20 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 
-
+client.on('message', msg => {
+var prefix = "!!!";
+  var args = msg.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+if (msg.author.bot) return;
+if(msg.content.startsWith(prefix + "setstream")) {
+      if(msg.author.id != "429972030092476437") return msg.reply("هذا الامر لصحاب الحساب");
+      client.user.setGame(`${argresult}`,"http://twitch.tv/S-F")
+      var Die = new Discord.RichEmbed()
+      .setTitle("✅تم تغير الستريمنق الخاص بك")
+     .addField("New Stream",`${argresult}`, true)
+     msg.channel.sendEmbed(Die);
+}
+});
 
 
 
